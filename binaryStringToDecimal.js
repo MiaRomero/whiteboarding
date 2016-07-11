6a4c21fa4b8af9f05316953a545d70413bbd8f3f
Alex Weinstein <alex94040@users.noreply.github.com>
2016-07-10 22:39:10 -0700

    Update binaryStringToDecimal.js

--- binaryStringToDecimal.js
+++ binaryStringToDecimal.js
@@ -1,12 +1,3 @@
-assertEquals(convertBinaryStringToDecimal('0'), 0);
-assertEquals(convertBinaryStringToDecimal('1'), 1);
-assertEquals(convertBinaryStringToDecimal('101'), 5);
-assertEquals(convertBinaryStringToDecimal('0011'), 3);
-assertEquals(convertBinaryStringToDecimal('1000'), 8);
-assertEquals(convertBinaryStringToDecimal('2'), -1); // error case: return -1
-assertEquals(convertBinaryStringToDecimal('-10'), -1); // error case
-assertEquals(convertBinaryStringToDecimal([1, 10]), -1); // error case
-assertEquals(convertBinaryStringToDecimal('abc'), -1);
 assertEquals(binaryTheHardWay('0'), 0);
 assertEquals(binaryTheHardWay('1'), 1);
 assertEquals(binaryTheHardWay('101'), 5);
@@ -16,14 +7,6 @@ assertEquals(binaryTheHardWay('2'), -1); // error case: return -1
 assertEquals(binaryTheHardWay('-10'), -1); // error case
 assertEquals(binaryTheHardWay([1, 10]), -1); // error case
 assertEquals(binaryTheHardWay('abc'), -1);
-
-
-function convertBinaryStringToDecimal(input) {
-  if (typeof input !== 'string') return -1;
-  var decimal = parseInt(input, 2);
-  if (decimal >= 0) return decimal;
-  return -1;
-}
 
 function binaryTheHardWay(input) {
   if (typeof input !== 'string' || input < 0) return -1;
